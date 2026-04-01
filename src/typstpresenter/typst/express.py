8742d8b24808005b853d43b7c532befd17f152f2
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-01 17:11:41 +0200

    Stub express function

---
+++ src/typstpresenter/typst/express.py
@@ -0,0 +1,8 @@
+from typstpresenter.model.Presentation import Presentation
+
+
+def express(presentation: Presentation) -> str:
+    """
+    Express a presentation represented in an abstract format as a Typst string.
+    """
+    raise NotImplementedError()
