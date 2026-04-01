d58250e46c2caf7713eb497dbf5975f7d63f52c1
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-01 16:10:46 +0200

    Improve interpreting title

--- src/typstpresenter/powerpoint/interpret.py
+++ src/typstpresenter/powerpoint/interpret.py
@@ -1,8 +1,12 @@
 from pptx.enum.shapes import PP_PLACEHOLDER_TYPE
+from pptx.shapes import Subshape
 from pptx.shapes.base import BaseShape
 from pptx.shapes.placeholder import SlidePlaceholder
+from pptx.text.text import _Paragraph, TextFrame, _Run
 
 from typstpresenter.model.Element import Element
+from typstpresenter.model.text.Link import Link
+from typstpresenter.model.text.Text import Text, Atom
 from typstpresenter.model.Title import Title
 from typstpresenter.powerpoint.Ignore import Ignore
 
@@ -18,16 +22,37 @@ def _interpret_placeholder(shape: SlidePlaceholder) -> Element | Ignore | None:
 
     match shape.placeholder_format.type:
         case PP_PLACEHOLDER_TYPE.TITLE:
-            return Title(text=shape.text)
+            return Title(text=_interpret_text_frame(shape.text_frame))
         case PP_PLACEHOLDER_TYPE.SLIDE_NUMBER:
             return Ignore()
         case _:
             return None
 
 
-def interpret(shape: BaseShape) -> Element | None:
+def _interpret_text_frame(text_frame: TextFrame) -> Text:
+    if len(text_frame.paragraphs) == 1:
+        return _interpret_paragraph(text_frame.paragraphs[0])
+    else:
+        raise NotImplementedError()  # TODO Implement as needed
+
+
+def _interpret_paragraph(paragraph: _Paragraph) -> Text:
+    # TODO Keep formatting, by going over the runs in the paragraph
+    return Text(
+        tuple(_interpret_run(run) for run in paragraph.runs)
+    )
+
+
+def _interpret_run(run: _Run) -> Atom:
+    if run.hyperlink.address is not None:
+        return Link(text=Text(run.text), target=run.hyperlink.address)
+
+    return run.text
+
+
+def interpret(shape: BaseShape | Subshape) -> Element | None:
     """
-    Interpret a shape coming from a PowerPoint.
+    Interpret a shape (or a subshape, which is an element introduced as child of a shape) coming from a PowerPoint.
 
     Interpreting a shape means converting it to an Element, i.e., an instance from our abstraction layer which
     we control. If a shape cannot be interpreted, this returns None.
@@ -36,4 +61,5 @@ def interpret(shape: BaseShape) -> Element | None:
         case SlidePlaceholder():
             return _interpret_placeholder(shape)
         case _:
+            # TODO Implement as needed
             return None
