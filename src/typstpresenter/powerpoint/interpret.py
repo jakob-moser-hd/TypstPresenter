bea03278ddd7a4ca39508c84a7d4f20c0d3ab561
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-01 17:11:32 +0200

    Reformat file

--- src/typstpresenter/powerpoint/interpret.py
+++ src/typstpresenter/powerpoint/interpret.py
@@ -38,9 +38,7 @@ def _interpret_text_frame(text_frame: TextFrame) -> Text:
 
 def _interpret_paragraph(paragraph: _Paragraph) -> Text:
     # TODO Keep formatting, by going over the runs in the paragraph
-    return Text(
-        tuple(_interpret_run(run) for run in paragraph.runs)
-    )
+    return Text(tuple(_interpret_run(run) for run in paragraph.runs))
 
 
 def _interpret_run(run: _Run) -> Atom:
