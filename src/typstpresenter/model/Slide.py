6622f275dbee83bf857b3bc81dd5d53af49f4b82
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-01 12:53:34 +0200

    Interpret shapes when deserializing

--- src/typstpresenter/model/Slide.py
+++ src/typstpresenter/model/Slide.py
@@ -1,9 +1,50 @@
+from collections.abc import Sequence
 from dataclasses import dataclass
-from typing import Self, Any
+from functools import cached_property
+from typing import Self
+
+import pptx.slide
+from pptx.shapes.base import BaseShape
+
+from typstpresenter.model.Element import Element
+from typstpresenter.model.Title import Title
+from typstpresenter.powerpoint.flatten import flatten
+from typstpresenter.powerpoint.interpret import interpret
 
 
-@dataclass
+@dataclass(frozen=True)
 class Slide:
+    elements: Sequence[Element]
+    shapes: Sequence[BaseShape]
+
+    @cached_property
+    def title(self) -> Title | None:
+        return self.get_singular_element_or_none(Title)
+
+    def get_singular_element_or_none[T: Element](self, t: type[T]) -> T | None:
+        elements = tuple(e for e in self.elements if isinstance(e, t))
+
+        if not elements:
+            return None
+
+        if len(elements) > 1:
+            raise ValueError(
+                f"Slide had {len(elements)} elements of type {t}, but should have either 0 or 1."
+            )
+
+        return elements[0]
+
     @classmethod
-    def from_pptx_slide(cls, pptx_slide: Any) -> Self:
-        return cls()
+    def from_pptx_slide(cls, pptx_slide: pptx.slide.Slide) -> Self:
+        # pptx_slide.slide_layout.name contains the layout name, which is often rather useless, as it can be
+        # "Titel und Inhalt" irrespective of whether the slide contains structured bullet-points or completely
+        # free floating thought bubbles.
+
+        elements_and_shapes = tuple(
+            interpret(shape) or shape for shape in flatten(pptx_slide.shapes)
+        )
+
+        return cls(
+            elements=tuple(x for x in elements_and_shapes if isinstance(x, Element)),
+            shapes=tuple(x for x in elements_and_shapes if isinstance(x, BaseShape)),
+        )
