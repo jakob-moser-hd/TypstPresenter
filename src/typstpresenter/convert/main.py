7051fba5449a483b3237fa7f33808fde5b6465e2
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-23 20:03:59 +0200

    Make output path configurable

--- src/typstpresenter/convert/main.py
+++ src/typstpresenter/convert/main.py
@@ -12,7 +12,7 @@ app = typer.Typer()
 logger = logging.getLogger(__name__)
 
 
-def convert_single(pptx_path: Path, maybe_compiler: Compiler | None) -> None:
+def convert_single(pptx_path: Path, maybe_compiler: Compiler | None, output_path: Path | None) -> None:
     """
     Convert a single *.pptx file to a *.typ file.
 
@@ -20,7 +20,8 @@ def convert_single(pptx_path: Path, maybe_compiler: Compiler | None) -> None:
     """
     presentation = Presentation.from_file(pptx_path)
 
-    typst_path = pptx_path.with_suffix(".typ")
+    typst_path = (pptx_path if not output_path else output_path / pptx_path.relative_to(pptx_path.parent)).with_suffix(".typ")
+
     presentation.to_file(typst_path)
 
     if maybe_compiler:
@@ -36,7 +37,11 @@ def convert(
     compile: Annotated[
         bool,
         typer.Option(help="If the final presentation *.pdf should also automatically be built from the PDF"),
-    ]
+    ],
+    output_path: Annotated[
+        Path | None,
+        typer.Option("--output-path", "-o", help="Path to a directory in which all output files should be stored")
+    ] = None,
 ) -> None:
     """
     Convert a given presentation or set of presentations from PPTX to Typst.
@@ -45,5 +50,8 @@ def convert(
     """
     maybe_compiler = Compiler() if compile else None
 
+    if output_path:
+        output_path.mkdir(parents=True, exist_ok=True)
+
     with multiprocessing.Pool() as p:
-        p.starmap(convert_single, ((path, maybe_compiler) for path in pptx_paths))
+        p.starmap(convert_single, ((path, maybe_compiler, output_path) for path in pptx_paths))
