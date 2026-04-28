f095919f37a8b033f7e67cdb34a2bb957eb5f32e
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-28 13:52:38 +0200

    Make Expressor generic

--- src/typstpresenter/typst/expressors/Expressor.py
+++ src/typstpresenter/typst/expressors/Expressor.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-from typing import Protocol, Callable, Any
+from typing import Protocol, Callable, Any, Generic
 
 from typstpresenter.model.Element import Element
 
 
-class Expressor(Protocol):
+class Expressor[T: Element | str | None](Protocol):
     def can_express(self, element: Element | str | None) -> bool:
         ...
 
-    def __call__(self, element: Element | str | None, express: Callable[[Element | str | None], str], context: Any) -> str:
+    def __call__(self, element: T, express: Callable[[Element | str | None], str], context: Any) -> str:
         ...
