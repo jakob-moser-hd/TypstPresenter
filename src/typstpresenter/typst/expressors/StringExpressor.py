70c50637141a3f526590726872865032237842ad
Jakob Moser <moser@cl.uni-heidelberg.de>
2026-04-28 13:52:43 +0200

    Escape at signs

--- src/typstpresenter/typst/expressors/StringExpressor.py
+++ src/typstpresenter/typst/expressors/StringExpressor.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
+from functools import reduce
 from typing import Any, Callable
 
 from typstpresenter.model.Element import Element
+
+
+CHARS_TO_ESCAPE = ("*", "~", "[", "]")
 
 
 class StringExpressor:
     def can_express(self, element: Element | str | None) -> bool:
         return isinstance(element, str)
 
-    def __call__(self, element: Any, express: Callable[[Element | str | None], str], context: Any) -> str:
-        # TODO Improve escaping logic
-        escaped = element.replace("*", r"\*").replace("~", r"\~")
-        escaped = escaped.replace("[", r"\[").replace("]", r"\]")
-        return escaped
+    def __call__(self, element: str, express: Callable[[Element | str | None], str], context: Any) -> str:
+        # If it was hard to write, it should be hard to read.
+        return reduce(lambda text, char: text.replace(char, rf"\{char}"), CHARS_TO_ESCAPE, element)
